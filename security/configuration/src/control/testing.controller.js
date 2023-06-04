31f094b454c6d4165da7c3970c1b9823b25e74c8
seintax <78845146+seintax@users.noreply.github.com>
2023-06-05 06:51:29 +0800

    upload for deployment

---
+++ security/configuration/src/control/testing.controller.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+router.get('/test', async (req, res) => {
+    console.log(req.query)
+    return res.status(200).json({
+        success: true,
+        message: "You have a successfull api call.",
+        parameters: req.query,
+    })
+})
+
+module.exports = router
