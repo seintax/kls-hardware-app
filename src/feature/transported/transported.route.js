1e8d0bf6c262adc79f036fb81370336784dad4b7
seintax <78845146+seintax@users.noreply.github.com>
2023-06-27 13:34:46 +0800

    Update transported.route.js

--- src/feature/transported/transported.route.js
+++ src/feature/transported/transported.route.js
@@ -83,4 +83,16 @@ router.get('/transported/transfer', async (req, res) => {
     })
 })
 
+router.get('/transported/inventory', async (req, res) => {
+    await service.inventoryRecord(req.query, (err, ans) => {
+        if (err) return res.status(200).json({
+            success: false, error: err
+        })
+        return res.status(200).json({
+            success: true,
+            result: ans || {},
+        })
+    })
+})
+
 module.exports = router
