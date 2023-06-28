6b4581176c1391cc1cb1e0d6b6aac6af4649a918
seintax <78845146+seintax@users.noreply.github.com>
2023-06-29 02:10:36 +0800

    Update reports.query.js

--- src/system/reports/reports.query.js
+++ src/system/reports/reports.query.js
@@ -29,8 +29,44 @@ const weeklySales = async (param, callback) => {
     })
 }
 
+const dailyReceivables = async (param, callback) => {
+    let sql = table.reports.dailyReceivables
+    my.query(sql, async (err, ans) => {
+        if (err) return callback(err)
+        return callback(null, ans)
+    })
+}
+
+const receivableCollection = async (param, callback) => {
+    let sql = table.reports.receivableCollection.replaceAll("@fr", param.fr).replaceAll("@to", param.to)
+    my.query(sql, async (err, ans) => {
+        if (err) return callback(err)
+        return callback(null, ans)
+    })
+}
+
+const dailyInventory = async (param, callback) => {
+    let sql = table.reports.dailyInventory.replaceAll("@fr", param.fr)
+    my.query(sql, async (err, ans) => {
+        if (err) return callback(err)
+        return callback(null, ans)
+    })
+}
+
+const dailyReturn = async (param, callback) => {
+    let sql = table.reports.dailyReturn.replaceAll("@fr", param.fr).replaceAll("@to", param.to)
+    my.query(sql, async (err, ans) => {
+        if (err) return callback(err)
+        return callback(null, ans)
+    })
+}
+
 module.exports = {
     dailySales,
     dailySummary,
-    weeklySales
+    weeklySales,
+    dailyReceivables,
+    receivableCollection,
+    dailyInventory,
+    dailyReturn,
 }
